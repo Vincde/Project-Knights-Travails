fa83788b296ed14bcbc7c5b568554dca838765ff
Vincde <145802178+Vincde@users.noreply.github.com>
2024-06-30 13:28:34 +0200

    //

--- src/index.js
+++ src/index.js
@@ -26,18 +26,32 @@ function graphCreation(possibleMoves) {
   return graph;
 }
 
-function searchResult(start, end, graph, res = [], queue = [], count = []) {
+function searchResult(start, end, graph, res = [], queue = []) {
   if (end[0] === start[0] && end[1] === start[1]) return "ur here already";
 
   const i = start[0] * 7 + start[0] + start[1];
   for (let j = 0; j < graph[i].head.length; j++) {
-    queue.push(graph[i].head[j]);
+    if (queue.length <= 7) {
+      queue.push([graph[i].head[j]]);
+    }else{
+      queue[] //you should put every "child" into one array position at a time
+    }
     if (graph[i].head[j][0] === end[0] && graph[i].head[j][1] === end[1]) {
-      return [end[0], end[1]];
+      return res.push([end[0], end[1]]);
     }
   }
 
-  res.push(searchResult(queue.shift(), end, graph, res, queue));
+  if (res[0] !== null) {
+    res.shift();
+  }
+
+  for (let w = 0; w < res.length; w++) {
+    if (res[w] !== queue[0]) res.push(queue[0]);
+  }
+
+  searchResult(queue.shift(), end, graph, res, queue);
+
+  return res;
 }
 
 function main() {
@@ -53,7 +67,17 @@ function main() {
     [-1, -2],
   ];
 
+  const start = [0, 2];
+  const end = [5, 6];
+  const queue = [];
+  const count = [];
+  let res = [];
+
+  res.push(start);
+
   graph = graphCreation(possibleMoves);
+  res = searchResult(start, end, graph, res, queue, count);
+  console.log(res);
   console.log(graph);
 }
 
